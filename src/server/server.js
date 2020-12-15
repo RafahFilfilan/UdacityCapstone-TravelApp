d9a0c97be07d3029e4bcc421aa2b2b30981f223c
Rafah <RafahFilfilan@gmail.com>
2020-12-15 23:57:27 +0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -2,17 +2,23 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var path = require('path')
+// Require Express to run server and routes
 const express = require('express')
+/* Dependencies */
 const bodyParser = require('body-parser')
+// Cors for cross origin allowance
 const cors = require('cors')
-
+// Start up an instance of app
 const app = express()
 const fetch = require('node-fetch')
 
+/* Middleware */
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Initialize the main project folder
 app.use(express.static('dist'))
 
 console.log(__dirname)
