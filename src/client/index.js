5fe6534ad3da95b24a8a6cbf2a796e089d670de3
Rafah <RafahFilfilan@gmail.com>
2020-12-15 23:54:34 +0300

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,13 @@
 import { handleSubmit } from './js/app'
 
+
+// Importing SASS files.
 import './styles/style.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 
-
+// It is required in this project to use .addEventListener() and here is the appropriate place.
 document.getElementById('submit').addEventListener('click', handleSubmit);
 
 export {
